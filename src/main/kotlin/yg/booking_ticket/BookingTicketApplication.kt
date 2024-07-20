dc2937372851cc97c5a1bb7218046e2261db4ffd
Kim yonggwan <sssw0101@naver.com>
2024-07-20 13:40:28 +0900

    feat: project init

---
+++ src/main/kotlin/yg/booking_ticket/BookingTicketApplication.kt
@@ -0,0 +1,11 @@
+package yg.booking_ticket
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BookingTicketApplication
+
+fun main(args: Array<String>) {
+	runApplication<BookingTicketApplication>(*args)
+}
---
+++ src/test/kotlin/yg/booking_ticket/BookingTicketApplicationTests.kt
@@ -0,0 +1,13 @@
+package yg.booking_ticket
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BookingTicketApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
