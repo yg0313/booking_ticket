12c10a6d140d28ce0d12c7b900d8825a7ae78722
Kim yonggwan <sssw0101@naver.com>
2024-08-12 22:11:26 +0900

    feat: init swagger file

---
+++ src/main/resources/static/swagger-initializer.js
@@ -0,0 +1,23 @@
+window.onload = function() {
+  //<editor-fold desc="Changeable Configuration Block">
+
+  // the following lines will be replaced by docker/configurator, when it runs in a docker-container
+  window.ui = SwaggerUIBundle({
+    urls: [
+		{"name" : "Start Api", "url" : "../api/start-api.yaml"},
+		{"name" : "사용자 및 인증", "url" : "../api/user-api.yaml"}
+	],
+    dom_id: '#swagger-ui',
+    deepLinking: true,
+    presets: [
+      SwaggerUIBundle.presets.apis,
+      SwaggerUIStandalonePreset
+    ],
+    plugins: [
+      SwaggerUIBundle.plugins.DownloadUrl
+    ],
+    layout: "StandaloneLayout"
+  });
+
+  //</editor-fold>
+};
